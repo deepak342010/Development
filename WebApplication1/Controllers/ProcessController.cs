9c051d80bc4056fa12fe4208dfe557244ba5b0cb
deepak342010 <deepak342010@gmail.com>
2021-05-12 19:43:09 +0530

    chnages

--- WebApplication1/Controllers/ProcessController.cs
+++ WebApplication1/Controllers/ProcessController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +36,61 @@ namespace WebApplication1.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(tblProcess);
+        }
+        public IActionResult Edit(int? processId)
+        {
+            if (processId == null)
+                return NotFound();
+
+            var process = _context.TblProcess.Find(processId);
+            if (process == null)
+                return NotFound();
+
+            return View(process);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int processId, TblProcess tblProcess)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblProcess);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProcessExists(tblProcess.IN_PROCESS_ID))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblProcess);
+        }
+        public IActionResult Details(int? processId)
+        {
+            if (processId == null)
+            {
+                return NotFound();
+            }
+
+            var process = _context.TblProcess
+                .FirstOrDefault(m => m.IN_PROCESS_ID == processId);
+            if (process == null)
+            {
+                return NotFound();
+            }
+
+            return View(process);
+        }
+
+
+        private bool ProcessExists(int id)
+        {
+            return _context.TblProcess.Any(e => e.IN_PROCESS_ID == id);
         }
     }
 }
